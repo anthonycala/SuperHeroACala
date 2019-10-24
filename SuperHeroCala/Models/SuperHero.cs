768cb852ffe16dd054bf973adfcf65f6d49bc6c6
unknown <antcala@icloud.com>
2019-10-24 14:34:27 -0500

    added migrations, and created the SuperHero Controller

--- SuperHeroCala/Models/SuperHero.cs
+++ SuperHeroCala/Models/SuperHero.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace SuperHeroCala.Models
 {
-    public class Person
+    public class SuperHero
     {
+        [Key]
+        public int ID { get; set; }
+        public string superHeroName { get; set; }
+        public string alterEgo { get; set; }
+        public string primarySuperHeroAbility { get; set; }
+        public string secondarySuperHeroAbility { get; set; }
+        public string catchPhrase { get; set; }
+
+
     }
 }
